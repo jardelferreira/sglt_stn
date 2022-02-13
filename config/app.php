9d8e300dbafa82671d57b13ed34ac3ec1294d115
jardelferreira <jardelferreira.sousa@gmail.com>
2022-02-13 09:23:45 -0300

    add ACL package and AdminLTE3 package

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Yajra\Acl\AclServiceProvider::class,
 
         /*
          * Application Service Providers...
